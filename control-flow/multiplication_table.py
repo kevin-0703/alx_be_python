7b92547bf7ec408cbf40d4d27412a7ece0efd8eb
KEVIN NSHUTI <nkevin881@gmail.com>
2025-06-02 14:13:26 +0200

    re-arranging and correction

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = input("Enter a number to generate its multiplication table: "  );
+number = input("Enter a number to see its multiplication table: "  );
 for i in range(1, 11):
     result = int(number) * i
     print(f"{number} x {i} = {result}")
